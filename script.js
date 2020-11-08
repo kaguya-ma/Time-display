ccb717432549373b0927b1fc06e918366d49ad46
Ma Tester <lulamae1983@gmail.com>
2020-11-08 18:08:04 +0000

    Added zeros to digital pad

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function clock() {
   let date = new Date()
-  let hour = date.getHours()
-  let minutes = date.getMinutes()
-  let seconds = date.getSeconds()
+  let hour = `${date.getHours()}`.padStart(2, '0')
+  let minutes = `${date.getMinutes()}`.padStart(2, '0')
+  let seconds = `${date.getSeconds()}`.padStart(2, '0')
   document.querySelector("#clock").innerHTML=`${hour}:${minutes}:${seconds}`
   
 }
